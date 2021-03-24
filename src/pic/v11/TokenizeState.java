81bff5e20f1feadfcabed4d45856e9f7266d6f71
Irwin <rodriguez.irwin@gmail.com>
2021-03-24 21:01:34 +0100

    Parsing Statements and Block Statements

---
+++ src/pic/v11/TokenizeState.java
@@ -0,0 +1,8 @@
+package pic.v11;
+
+public enum TokenizeState {
+	DEFAULT,
+	OPERATOR,
+	NUMBER,
+	KEYWORD,
+}
---
+++ src/pic/v11/WaitNode.java
@@ -0,0 +1,24 @@
+package pic.v11;
+
+public class WaitNode extends Node {
+	public Node interval;
+	
+	public WaitNode() {
+		// Nothing
+	}
+	
+	public WaitNode(Node interval) {
+		this.interval = interval;
+	}
+	
+	public Object eval() {
+		Integer waitAmount = Integer.valueOf(interval.eval().toString());
+		
+		try {
+			Thread.sleep(waitAmount.intValue());
+		} catch(Exception e) {
+			System.out.println("Error in WaitNode.eval() method: " + e);
+		}
+		return waitAmount;
+	}
+}
