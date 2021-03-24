c9bcb25b3efe013b7659a0971d942741811f3885
Irwin <rodriguez.irwin@gmail.com>
2021-03-24 21:52:20 +0100

    Parsing Variables: this technique uses an unique symbol table embedbed in the Parser class which means the interpreter will not be able to interpret nested functions. I'll fix this when finish the passthrough of the book.

---
+++ src/pic/v13/Util.java
@@ -0,0 +1,33 @@
+package pic.v13;
+
+import java.util.List;
+
+public class Util {
+	public static void Write(Object obj) {
+		System.out.println(obj);
+	}
+	
+	public static void Writeln(Object obj) {
+		System.out.println(obj);
+	}
+	
+	public static void Writeln() {
+		System.out.println();
+	}
+
+	public static void PrettyPrint(List<Token> tokens) {
+		int numberCount = 0;
+		int opCount = 0;
+		for (Token token : tokens) {
+			if (token.type == TokenType.NUMBER) {
+				System.out.println("Number....: " + token.text);
+				numberCount += 1;
+			} else {
+				System.out.println("Operator..: " + token.type);
+				opCount += 1;
+			}
+		}
+		System.out.println("You have got " + numberCount + 
+				" different number and " + opCount + " operators.");
+	}
+}
